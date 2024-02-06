d799bfb611862e5ba08c7e0110465c0ccb7cf2d9
Peter Cooper <git@peterc.org>
2024-02-06 22:30:32 +0000

    Finished HTML cleaning

--- lib/sofetch.rb
+++ lib/sofetch.rb
@@ -37,9 +37,27 @@ module Sofetch
       end
     end
 
+    def to_hash
+      @hash ||= {
+        url: @url,
+        site_name: site_name,
+        titles: titles,
+        descriptions: descriptions,
+        authors: authors,
+        opengraph: opengraph,
+        metas: metas,
+        headings: headings,
+        paragraphs: paragraphs,
+        html: html,
+        resolved_url: resolved_url,
+        feeds: feeds,
+        published_at: published_at
+      }
+    end
+
     def opengraph
       raise Sofetch::Error, "No data available" unless @raw_data
-      @raw_data[:metadata]["opengraph"]&.first
+      @opengraph ||= @raw_data[:metadata]["opengraph"]&.first
     end
 
     def metas
@@ -53,7 +71,7 @@ module Sofetch
     end
 
     def paragraphs
-      html_document.css("p").map(&:text).map(&:strip).reject(&:empty?)
+      html_document.css("p").map(&:text).map(&:strip).reject(&:empty?).first(10)
     end
 
     def html
@@ -65,6 +83,17 @@ module Sofetch
       raw_data[:"resolved-url"]
     end
 
+    def feeds
+      raise Sofetch::Error, "No data available" unless @raw_data
+      feeds = []
+      feeds << html_document.at_css("link[type='application/rss+xml']")&.attributes&.dig("href")&.value
+      feeds << html_document.at_css("link[type='application/atom+xml']")&.attributes&.dig("href")&.value
+      feeds << html_document.css("a").map { |a| a.attributes["href"]&.value }.compact.find { |href| href&.match?(/rss|feed/) }
+      # If any of the feeds are relative, make them absolute
+      feeds.map! { |feed| URI.join(resolved_url, feed).to_s if feed }
+      feeds.compact.uniq.map(&:strip)
+    end
+
     def published_at
       raise Sofetch::Error, "No data available" unless @raw_data
       published_ats = []
@@ -73,6 +102,8 @@ module Sofetch
       if resolved_url && resolved_url[/\d{4}\/\d{2}\/\d{2}/]
         published_ats << resolved_url[/\d{4}\/\d{2}\/\d{2}/]
       end
+      published_ats << html_document.at_css("[class*='date'], [id*='date'], time[datetime]")&.text
+      published_ats << html_document.at_css("time[itemprop='datePublished']")&.attributes&.dig("datetime")&.value
       published_ats.compact.uniq.map(&:strip)      
     end
 
@@ -106,11 +137,74 @@ module Sofetch
       authors = []
       authors << opengraph["article:author"] if opengraph
       authors << metas["author"] if metas
+      authors << html_document.at_css(".p-author")&.text
       authors.compact.uniq.map(&:strip)
     end
 
     def html_document
-      Nokogiri::HTML(html)
+      @html_document ||= Nokogiri::HTML(html)
+    end
+
+    def clean_html_document
+      html_document.css("script, style").remove
+      # Remove all elements that have a hidden attribute
+      html_document.css("[hidden]").remove
+      # Remove all elements with style attributes that contain display: none
+      html_document.css("[style*='display: none']").remove
+      # Remove all comments
+      html_document.xpath("//comment()").remove
+      # Remove all DIV, LI, SPAN and P elements that are empty
+      html_document.css("div, li, span, p").each do |el|
+        el.remove if el.text.strip.empty?
+      end
+
+      # Remove all these elements which tend to not have content
+      html_document.css("style, link, script, aside, footer, button, svg, label, nav, textarea, noscript, iframe, form, input, img, image, select, option, picture, figure, figcaption, menu").remove
+
+      # Remove elements with these IDs ['header', 'footer', 'site-header', 'site-footer', 'cookie-banner', 'outdated']
+      html_document.css("#header, #footer, #site-header, #site-footer, #cookie-banner, #outdated").remove
+
+      # Remove elements with these classes ['message-bar', 'tag', 'adwrap', 'sidebar']
+      html_document.css(".message-bar, .tag, .adwrap, .sidebar").remove
+
+      # To reduce size, clear all element class attributes that are
+      # longer than 20 characters in all
+      html_document.css("*").each do |el|
+        el.remove_attribute("class") if el.attributes["class"]&.value&.size.to_i > 20
+        el.remove_attribute("data")
+      end
+
+      # Remove all elements that have classes or IDs containing certain words that imply they are not content
+      partial_class_names = ['related', 'cookie', 'consent', 'sticky', 'share', 'sr-only']
+      partial_class_names.each do |partial_class_name|
+        html_document.css("*[class*='#{partial_class_name}']").remove
+      end
+
+      # Remove head tags
+      html_document.css("head").remove
+
+      # Remove all span tags but keep the content
+      html_document.css("span").each do |el|
+        el.replace(el.text)
+      end
+
+      # Remove all block elements that are almost entirely made up of <a> elements
+      html_document.css("div, article, aside, p").each do |el|
+        next if el.text.size < 100
+        a_count = el.css("a").size
+        if a_count > 4 && a_count > el.text.size / 10
+          el.remove
+        end
+      end
+
+      # Remove all elements that contain no other elements and have no text
+      html_document.css("*").each do |el|
+        next if el.text.size > 2
+        next if el.children.size > 0
+        el.remove
+      end
+
+      html_document
     end
 
     def titles
@@ -175,7 +269,6 @@ module Sofetch
       if res.code == '200' && res.body.start_with?('{')
         body = JSON.parse(res.body)
         body = body.slice('headers', 'type', 'cost', 'initial-status-code', 'resolved-url', 'metadata', 'body')
-        # Convert keys to symbols
         body = body.transform_keys(&:to_sym)
         return { code: status_code, success: true }.merge(body)
       end
